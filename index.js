74b064aa301e060b16805e6a2bf344d50e0bd3e0
cmd-mish <popov.mikhail.al@gmail.com>
2023-06-18 23:51:42 +0300

    Create simple response

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-const url = "https://aws.amazon.com/";
+import { format } from 'date-fns';
+import { utcToZonedTime } from 'date-fns-tz';
+
+const ENDPOINT = process.env.ENDPOINT;
 
 export const handler = async (event) => {
     try {
-        // fetch is available with Node.js 18
-        const res = await fetch(url);
-        console.info("status", res.status);
-        return res.status;
+        const now = new Date();
+        const date = format(now, 'yyyy-MM-dd');
+        const currentHour = utcToZonedTime(now, 'Europe/Helsinki').getHours();
+
+        const URI = `${ENDPOINT}/${date}/${date}?lang=en`;
+        const data = await fetch(URI);
+
+        if (data.ok) {
+            const json = await data.json();
+            const output = json.find(item => item.timeStampHour === `${currentHour}:00`);
+            return output;
+        }
+        return { statusCode: 500, body: 'Something went wrong' };
     }
     catch (e) {
         console.error(e);
